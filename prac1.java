7ab87a556fc6389e6ca19e78a1201ae7fc965dd8
unknown <pratapanuj378@gmail.com>
2024-08-12 16:37:28 +0530

    add new line

---
+++ prac1.java
@@ -0,0 +1,9 @@
+package trialProject;
+
+public  class prac1{
+    
+    public static void main(String[] s){
+        System.out.println("ANuj");
+        System.out.println("heheh");    
+    }
+}
